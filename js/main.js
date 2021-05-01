fde399df7150fdf21f258d4deafe4242e15d8ccb
Adrian Furlan <adrian.furlan.c@gmail.com>
2021-05-01 14:10:41 +0200

    Add sounds to red buttons

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function btnFireball() {
+    var audio = new Audio('../assets/audio/Fireball.mp3');
+    audio.volume = 0.8;
+    audio.play();
+}
+
+function btnJump() {
+    var audio = new Audio('../assets/audio/Jump.mp3');
+    audio.volume = 0.25;
+    audio.play();
+}
+
